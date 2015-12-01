8e9baf0c5c6a083368e685f58aeed258324d935a
karpathy <andrej.karpathy@gmail.com>
2015-11-30 17:42:27 -0800

    adding logging for tornado

--- serve.py
+++ serve.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
 import cPickle as pickle
 import numpy as np
 import json
@@ -181,6 +178,11 @@ if __name__ == "__main__":
   print 'starting!'
   if args.prod:
     # run on Tornado instead, since running raw Flask in prod is not recommended
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
+    from tornado.log import enable_pretty_logging
+    enable_pretty_logging()
     http_server = HTTPServer(WSGIContainer(app))
     http_server.listen(5000)
     IOLoop.instance().start()
