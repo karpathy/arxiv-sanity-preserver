a381a34d09f55121ab2c05c0dbfa10c786878710
karpathy <andrej.karpathy@gmail.com>
2016-02-14 13:05:43 -0800

    adding cs.NE and stat.ML categories as default

--- fetch_papers.py
+++ fetch_papers.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('--db_path', dest='db_path', type=str, default='db.p', help='database pickle filename that we enrich')
   parser.add_argument('--search_query', dest='search_query', type=str,
-                      default='cat:cs.CV+OR+cat:cs.LG+OR+cat:cs.CL',
+                      default='cat:cs.CV+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML',
                       help='query used for arxiv API. See http://arxiv.org/help/api/user-manual#detailed_examples')
   parser.add_argument('--start_index', dest='start_index', type=int, default=0, help='0 = most recent API result')
   parser.add_argument('--max_index', dest='max_index', type=int, default=10000, help='upper bound on paper index we will fetch')
