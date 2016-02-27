e200df5740cf9afc80b2b328078300b328a36cf2
karpathy <andrej.karpathy@gmail.com>
2016-02-27 12:48:26 -0800

    fixing a possible bug

--- buildsvm.py
+++ buildsvm.py
@@ -23,6 +23,9 @@ def query_db(query, args=(), one=False):
 users = query_db('''select * from user''')
 for u in users:
   print u
+print 'number of users: ', len(users)
+print 'PRESS ENTER TO CONTINUE'
+raw_input()
 
 def strip_version(idstr):
   """ identity function if arxiv id has no version, otherwise strips it. """
@@ -43,16 +46,15 @@ for u in users:
   uid = u['user_id']
   lib = query_db('''select * from library where user_id = ?''', [uid])
   pids = [x['paper_id'] for x in lib] # raw pids without version
-  posix = [xtoi[p] for p in pids]
+  posix = [xtoi[p] for p in pids if p in xtoi]
   
   if not posix:
     continue # empty library for this user maybe?
 
   print pids
-
   y = np.zeros(X.shape[0])
-  for p in pids: 
-    y[xtoi[p]] = 1
+  for ix in posix:
+    y[ix] = 1
 
   #__init__(penalty='l2', loss='squared_hinge', dual=True, tol=0.0001, C=1.0, multi_class='ovr', fit_intercept=True, intercept_scaling=1, class_weight=None, verbose=0, random_state=None, max_iter=1000)[source]
   clf = svm.LinearSVC(class_weight='auto', verbose=True, max_iter=10000, tol=1e-6, C=1)
