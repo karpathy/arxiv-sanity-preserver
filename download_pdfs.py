f38b71de8a821d9b51d4145ba7a2c9a89389edbf
OpenAI server <sys@openai.com>
2017-01-12 00:23:33 -0800

    converted download_pdfs to py3

--- download_pdfs.py
+++ download_pdfs.py
@@ -1,41 +1,45 @@
-import cPickle as pickle
-import urllib2
+import pickle
+from  urllib.request import urlopen
 import shutil
 import time
 import os
 import random
 
-os.system('mkdir -p pdf') # ?
-
 timeout_secs = 10 # after this many seconds we give up on a paper
+
+pdf_dir = os.path.join('data', 'pdf')
+db_file = os.path.join('data', 'db.p')
+if not os.path.exists(pdf_dir): os.makedirs(pdf_dir)
+
 numok = 0
 numtot = 0
-db = pickle.load(open('db.p', 'rb'))
-have = set(os.listdir('pdf')) # get list of all pdfs we already have
-for pid,j in db.iteritems():
+db = pickle.load(open(db_file, 'rb'))
+have = set(os.listdir(pdf_dir)) # get list of all pdfs we already have
+for pid,j in db.items():
   
   pdfs = [x['href'] for x in j['links'] if x['type'] == 'application/pdf']
   assert len(pdfs) == 1
   pdf_url = pdfs[0] + '.pdf'
   basename = pdf_url.split('/')[-1]
-  fname = os.path.join('pdf', basename)
+  fname = os.path.join(pdf_dir, basename)
 
   # try retrieve the pdf
   numtot += 1
   try:
     if not basename in have:
-      print 'fetching %s into %s' % (pdf_url, fname)
-      req = urllib2.urlopen(pdf_url, None, timeout_secs)
+      print('fetching %s into %s' % (pdf_url, fname))
+      req = urlopen(pdf_url, None, timeout_secs)
       with open(fname, 'wb') as fp:
           shutil.copyfileobj(req, fp)
       time.sleep(0.1 + random.uniform(0,0.2))
     else:
-      print '%s exists, skipping' % (fname, )
+      print('%s exists, skipping' % (fname, ))
     numok+=1
-  except Exception, e:
-    print 'error downloading: ', pdf_url
-    print e
+  except Exception as e:
+    print('error downloading: ', pdf_url)
+    print(e)
   
-  print '%d/%d of %d downloaded ok.' % (numok, numtot, len(db))
+  print('%d/%d of %d downloaded ok.' % (numok, numtot, len(db)))
   
-print 'final number of papers downloaded okay: %d/%d' % (numok, len(db))
+print('final number of papers downloaded okay: %d/%d' % (numok, len(db)))
+
