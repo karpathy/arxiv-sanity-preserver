ef67649c414a137154c96193e1778ac274eae3f2
Andrej Karpathy <andrej.karpathy@gmail.com>
2017-01-12 00:37:07 -0800

    converted parse_pdf_to_text to py3

--- parse_pdf_to_text.py
+++ parse_pdf_to_text.py
@@ -1,35 +1,43 @@
 """
-Very simple script that simply iterates over all files pdf/f.pdf
-and create a file txt/f.pdf.txt that contains the raw text, extracted
+Very simple script that simply iterates over all files data/pdf/f.pdf
+and create a file data/txt/f.pdf.txt that contains the raw text, extracted
 using the "pdftotext" command. If a pdf cannot be converted, this
 script will not produce the output file.
 """
 
-import cPickle as pickle
-import urllib2
-import shutil
+import pickle
 import time
 import os
-import random
+import shutil
+import sys
 
-os.system('mkdir -p txt') # ?
+# make sure pdftotext is installed
+if not shutil.which('pdftotext'): # needs Python 3.3+
+  print('ERROR: you don\'t have pdftotext installed. Install it first before calling this script')
+  sys.exit()
 
-have = set(os.listdir('txt'))
-files = os.listdir('pdf')
+txt_folder = os.path.join('data', 'txt')
+pdf_folder = os.path.join('data', 'pdf')
+if not os.path.exists(txt_folder): os.makedirs(txt_folder)
+
+have = set(os.listdir(txt_folder))
+files = os.listdir(pdf_folder)
 for i,f in enumerate(files, start=1):
-  pdf_path = os.path.join('pdf', f)
+  pdf_path = os.path.join(pdf_folder, f)
   txt_basename = f + '.txt'
-  txt_path = os.path.join('txt', txt_basename)
+  txt_path = os.path.join(txt_folder, txt_basename)
   if not txt_basename in have:
     cmd = "pdftotext %s %s" % (pdf_path, txt_path)
     os.system(cmd)
-    print '%d/%d %s' % (i, len(files), cmd)
+    print('%d/%d %s' % (i, len(files), cmd))
 
     # check output was made
     if not os.path.isfile(txt_path):
       # there was an error with converting the pdf
+      print('there was a problem with parsing %s to text, creating an empty text file.' % (pdf_path, ))
       os.system('touch ' + txt_path) # create empty file, but it's a record of having tried to convert
 
     time.sleep(0.02) # silly way for allowing for ctrl+c termination
   else:
-    print 'skipping %s, already exists.' % (pdf_path, )
+    print('skipping %s, already exists.' % (pdf_path, ))
+
