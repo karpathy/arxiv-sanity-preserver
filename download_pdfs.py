4f836724288488e5b200091910aee2c57d6a66c1
Gregory King <greg.james.king@gmail.com>
2017-01-13 14:52:42 -0800

    Updating workflow for python3 compatiblity; (1) added print_function; (2) reworked pickle import; (3) reworked urllib import; (4) added download_pdf function to remove magic values;

--- download_pdfs.py
+++ download_pdfs.py
@@ -1,41 +1,62 @@
-import cPickle as pickle
-import urllib2
+from __future__ import print_function
+
+try:
+  from urllib.parse import urlparse, urlencode
+  from urllib.request import urlopen, Request
+  from urllib.error import HTTPError
+except ImportError:
+  from urlparse import urlparse
+  from urllib import urlencode
+  from urllib2 import urlopen, Request, HTTPError
+
+try:
+  import pickle as pickle
+except:
+  import cPickle as pickle
+
 import shutil
 import time
 import os
 import random
 
-os.system('mkdir -p pdf') # ?
+def download_pdf(paper_timeout=10,#after this many seconds we give up on a paper
+                 database_file='db.p',#database file
+                 out_dir='pdf'):
+  os.system('mkdir -p pdf') # ?
+  timeout_secs = paper_timeout # after this many seconds we give up on a paper
+  numok = 0
+  numtot = 0
+  db = pickle.load(open(database_file, 'rb'))
+  have = set(os.listdir(out_dir)) # get list of all pdfs we already have
+  for pid,j in db.items():
+    pdfs = [x['href'] for x in j['links'] if x['type'] == 'application/pdf']
+    assert len(pdfs) == 1
+    pdf_url = pdfs[0] + '.pdf'
+    basename = pdf_url.split('/')[-1]
+    fname = os.path.join(out_dir, basename)
 
-timeout_secs = 10 # after this many seconds we give up on a paper
-numok = 0
-numtot = 0
-db = pickle.load(open('db.p', 'rb'))
-have = set(os.listdir('pdf')) # get list of all pdfs we already have
-for pid,j in db.iteritems():
-  
-  pdfs = [x['href'] for x in j['links'] if x['type'] == 'application/pdf']
-  assert len(pdfs) == 1
-  pdf_url = pdfs[0] + '.pdf'
-  basename = pdf_url.split('/')[-1]
-  fname = os.path.join('pdf', basename)
+    # try retrieve the pdf
+    numtot += 1
+    try:
+      if not basename in have:
+        print('fetching %s into %s' % (pdf_url, fname))
+        req = urlopen(pdf_url, None, timeout_secs)
+        with open(fname, 'wb') as fp:
+            shutil.copyfileobj(req, fp)
+        time.sleep(0.1 + random.uniform(0,0.2))
+      else:
+        print('%s exists, skipping' % (fname, ))
+      numok += 1
+    except Exception as e:
+      print('error downloading: ', pdf_url)
+      print(e)
+    print('%d/%d of %d downloaded ok.' % (numok, numtot, len(db)))
+  print('final number of papers downloaded okay: %d/%d' % (numok, len(db)))
+  return numok, db
 
-  # try retrieve the pdf
-  numtot += 1
-  try:
-    if not basename in have:
-      print 'fetching %s into %s' % (pdf_url, fname)
-      req = urllib2.urlopen(pdf_url, None, timeout_secs)
-      with open(fname, 'wb') as fp:
-          shutil.copyfileobj(req, fp)
-      time.sleep(0.1 + random.uniform(0,0.2))
-    else:
-      print '%s exists, skipping' % (fname, )
-    numok+=1
-  except Exception, e:
-    print 'error downloading: ', pdf_url
-    print e
-  
-  print '%d/%d of %d downloaded ok.' % (numok, numtot, len(db))
-  
-print 'final number of papers downloaded okay: %d/%d' % (numok, len(db))
+def main():
+  print("Starting to download PDFs")
+  download_pdf()
+
+if __name__ == '__main__':
+  main()
