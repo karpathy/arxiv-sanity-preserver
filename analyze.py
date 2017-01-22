9242113c0f9e43f04a433d924e520fdebdfd77e4
Andrej Karpathy <andrej.karpathy@gmail.com>
2017-01-21 17:07:52 -0800

    ported analyze.py to py3, but changes to what it computes are coming i think

--- analyze.py
+++ analyze.py
@@ -2,54 +2,54 @@
 Reads txt files of all papers and computes tfidf vectors for all papers.
 Dumps results to file tfidf.p
 """
-from sklearn.feature_extraction.text import TfidfVectorizer
-import cPickle as pickle
-import urllib2
-import shutil
-import time
 import os
-import random
+import pickle
+
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 import utils
 
 # read database
-db = pickle.load(open('db.p', 'rb'))
+db_path = os.path.join('data', 'db.p')
+db = pickle.load(open(db_path, 'rb'))
 
 # read all text files for all papers into memory
-txts = []
-pids = []
-n=0
-for pid,j in db.iteritems():
-  n+=1
+txts, pids = [], []
+n = 0
+for pid,j in db.items():
+  n += 1
   idvv = '%sv%d' % (j['_rawid'], j['_version'])
-  fname = os.path.join('txt', idvv) + '.pdf.txt'
-  if os.path.isfile(fname): # some pdfs dont translate to txt
-    txt = open(fname, 'r').read()
-    if len(txt) > 100: # way too short and suspicious
+  txt_path = os.path.join('data', 'txt', idvv) + '.pdf.txt'
+  if os.path.isfile(txt_path): # some pdfs dont translate to txt
+    txt = open(txt_path, 'r').read()
+    if len(txt) > 1000 and len(txt) < 500000: # 500K is VERY conservative upper bound
       txts.append(txt) # todo later: maybe filter or something some of them
       pids.append(idvv)
-      print 'read %d/%d (%s) with %d chars' % (n, len(db), idvv, len(txt))
+      print("read %d/%d (%s) with %d chars" % (n, len(db), idvv, len(txt)))
     else:
-      print 'skipped %d/%d (%s) with %d chars: suspicious!' % (n, len(db), idvv, len(txt))
+      print("skipped %d/%d (%s) with %d chars: suspicious!" % (n, len(db), idvv, len(txt)))
+print("in total read in %d text files out of %d db entries." % (len(txts), len(db)))
 
 # compute tfidf vectors with scikits
 v = TfidfVectorizer(input='content', 
         encoding='utf-8', decode_error='replace', strip_accents='unicode', 
         lowercase=True, analyzer='word', stop_words='english', 
         token_pattern=r'(?u)\b[a-zA-Z_][a-zA-Z0-9_]+\b',
-        ngram_range=(1, 2), max_features = 20000, 
-        norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=False)
+        ngram_range=(1, 2), max_features = 1000, 
+        norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=False,
+        max_df=1.0, min_df=1)
 
 X = v.fit_transform(txts)
-print v.vocabulary_
-print X.shape
+print(v.vocabulary_)
+print(X.shape)
 
 # write full matrix out
 out = {}
 out['X'] = X # this one is heavy!
-print('writing tfidf.p')
-utils.safe_pickle_dump(out, "tfidf.p")
+tfidf_path = os.path.join('data', 'tfidf.p')
+print("writing", tfidf_path)
+utils.safe_pickle_dump(out, tfidf_path)
 
 # writing lighter metadata information into a separate (smaller) file
 out = {}
@@ -57,21 +57,23 @@ out['vocab'] = v.vocabulary_
 out['idf'] = v._tfidf.idf_
 out['pids'] = pids # a full idvv string (id and version number)
 out['ptoi'] = { x:i for i,x in enumerate(pids) } # pid to ix in X mapping
-print('writing tfidf_meta.p')
-utils.safe_pickle_dump(out, "tfidf_meta.p")
+meta_path = 'tfidf_meta.p'
+print("writing", meta_path)
+utils.safe_pickle_dump(out, meta_path)
 
-print 'precomputing nearest neighbor queries in batches...'
+print("precomputing nearest neighbor queries in batches...")
 X = X.todense() # originally it's a sparse matrix
 sim_dict = {}
 batch_size = 200
-for i in xrange(0,len(pids),batch_size):
+for i in range(0,len(pids),batch_size):
   i1 = min(len(pids), i+batch_size)
   xquery = X[i:i1] # BxD
   ds = -np.asarray(np.dot(X, xquery.T)) #NxD * DxB => NxB
   IX = np.argsort(ds, axis=0) # NxB
-  for j in xrange(i1-i):
+  for j in range(i1-i):
     sim_dict[pids[i+j]] = [pids[q] for q in list(IX[:50,j])]
-  print '%d/%d...' % (i, len(pids))
+  print('%d/%d...' % (i, len(pids)))
 
-print('writing sim_dict.p')
-utils.safe_pickle_dump(sim_dict, "sim_dict.p")
+sim_path = 'sim_dict.p'
+print("writing", sim_path)
+utils.safe_pickle_dump(sim_dict, sim_path)
