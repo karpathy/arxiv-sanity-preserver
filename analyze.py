683677f1821e446e322b8006aea0bdac00a0839e
Andrej Karpathy <andrej.karpathy@gmail.com>
2017-02-06 19:45:22 -0800

    upgrading analyze.py to only train on subset of documents up to some fixed amount converse memory

--- analyze.py
+++ analyze.py
@@ -4,32 +4,38 @@ Dumps results to file tfidf.p
 """
 import os
 import pickle
+from random import shuffle, seed
 
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-import utils
+from utils import Config, safe_pickle_dump
+
+seed(1337)
+max_train = 10000 # max number of tfidf training documents (chosen randomly), for memory efficiency
 
 # read database
-db_path = 'db.p'
-db = pickle.load(open(db_path, 'rb'))
+db = pickle.load(open(Config.db_path, 'rb'))
 
 # read all text files for all papers into memory
-txts, pids = [], []
+txt_paths, pids = [], []
 n = 0
 for pid,j in db.items():
   n += 1
   idvv = '%sv%d' % (j['_rawid'], j['_version'])
   txt_path = os.path.join('data', 'txt', idvv) + '.pdf.txt'
   if os.path.isfile(txt_path): # some pdfs dont translate to txt
-    txt = open(txt_path, 'r').read()
+    with open(txt_path, 'r') as f:
+      txt = f.read()
     if len(txt) > 1000 and len(txt) < 500000: # 500K is VERY conservative upper bound
-      txts.append(txt) # todo later: maybe filter or something some of them
+      txt_paths.append(txt_path) # todo later: maybe filter or something some of them
       pids.append(idvv)
       print("read %d/%d (%s) with %d chars" % (n, len(db), idvv, len(txt)))
     else:
       print("skipped %d/%d (%s) with %d chars: suspicious!" % (n, len(db), idvv, len(txt)))
-print("in total read in %d text files out of %d db entries." % (len(txts), len(db)))
+  else:
+    print("could not find %s in txt folder." % (txt_path, ))
+print("in total read in %d text files out of %d db entries." % (len(txt_paths), len(db)))
 
 # compute tfidf vectors with scikits
 v = TfidfVectorizer(input='content', 
@@ -40,16 +46,33 @@ v = TfidfVectorizer(input='content',
         norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=True,
         max_df=1.0, min_df=1)
 
-X = v.fit_transform(txts)
+# create an iterator object to conserve memory
+def make_corpus(paths):
+  for p in paths:
+    with open(p, 'r') as f:
+      txt = f.read()
+    yield txt
+
+# train
+train_txt_paths = list(txt_paths) # duplicate
+shuffle(train_txt_paths) # shuffle
+train_txt_paths = train_txt_paths[:min(len(train_txt_paths), max_train)] # crop
+print("training on %d documents..." % (len(train_txt_paths), ))
+train_corpus = make_corpus(train_txt_paths)
+v.fit(train_corpus)
+
+# transform
+print("transforming %d documents..." % (len(txt_paths), ))
+corpus = make_corpus(txt_paths)
+X = v.transform(corpus)
 print(v.vocabulary_)
 print(X.shape)
 
 # write full matrix out
 out = {}
 out['X'] = X # this one is heavy!
-tfidf_path = os.path.join('data', 'tfidf.p')
-print("writing", tfidf_path)
-utils.safe_pickle_dump(out, tfidf_path)
+print("writing", Config.tfidf_path)
+safe_pickle_dump(out, Config.tfidf_path)
 
 # writing lighter metadata information into a separate (smaller) file
 out = {}
@@ -57,14 +80,13 @@ out['vocab'] = v.vocabulary_
 out['idf'] = v._tfidf.idf_
 out['pids'] = pids # a full idvv string (id and version number)
 out['ptoi'] = { x:i for i,x in enumerate(pids) } # pid to ix in X mapping
-meta_path = 'tfidf_meta.p'
-print("writing", meta_path)
-utils.safe_pickle_dump(out, meta_path)
+print("writing", Config.meta_path)
+safe_pickle_dump(out, Config.meta_path)
 
 print("precomputing nearest neighbor queries in batches...")
 X = X.todense() # originally it's a sparse matrix
 sim_dict = {}
-batch_size = 200
+batch_size = 500
 for i in range(0,len(pids),batch_size):
   i1 = min(len(pids), i+batch_size)
   xquery = X[i:i1] # BxD
@@ -74,6 +96,5 @@ for i in range(0,len(pids),batch_size):
     sim_dict[pids[i+j]] = [pids[q] for q in list(IX[:50,j])]
   print('%d/%d...' % (i, len(pids)))
 
-sim_path = 'sim_dict.p'
-print("writing", sim_path)
-utils.safe_pickle_dump(sim_dict, sim_path)
+print("writing", Config.sim_path)
+safe_pickle_dump(sim_dict, Config.sim_path)
