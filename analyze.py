4206d3cb9693d7a18296bb9c598156a46a1a90ed
Dabbrivia <diego@abbrivia.com>
2020-02-10 16:08:21 +0000

    Multiprocessing etc. for 200k articles

--- analyze.py
+++ analyze.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 """
 Reads txt files of all papers and computes tfidf vectors for all papers.
 Dumps results to file tfidf.p
@@ -10,6 +12,15 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from utils import Config, safe_pickle_dump
+from joblib import Parallel, delayed
+
+import multiprocessing
+import pandas as pd
+import numpy as np
+from multiprocessing import Pool
+import scipy.sparse as sp
+
+import regex
 
 seed(1337)
 max_train = 5000 # max number of tfidf training documents (chosen randomly), for memory efficiency
@@ -19,40 +30,64 @@ max_features = 5000
 db = pickle.load(open(Config.db_path, 'rb'))
 
 # read all text files for all papers into memory
+
+def read_txt_path(p):      
+  with open(p, 'r') as f:
+    try: # some problems with unicode may arize
+      txt = f.read()
+    except:
+      txt = "" 
+  return txt
+
 txt_paths, pids = [], []
 n = 0
 for pid,j in db.items():
   n += 1
   idvv = '%sv%d' % (j['_rawid'], j['_version'])
-  txt_path = os.path.join('data', 'txt', idvv) + '.pdf.txt'
+  idyymm = j['_rawid'][:4]
+
+  if j['_rawid'][:4].isdigit() and '.' in j['_rawid']: # this is the current scheme from 0707
+    schema='0707'
+    txt_path = os.path.join('data', 'txt'
+            , j['_rawid'][:4]
+            , j['_rawid']+'.txt') # YYMM/YYMM.xxxxx.pdf (number of xxx is variable)
+  elif '/' in j['_rawid']: #some rawids had the category and the id
+   shema='slash'
+   txt_path = os.path.join('data', 'txt'
+           , j['_rawid'].split("/")[1][:4].split("-")[0]
+           , "".join(j['_rawid'].split("/"))+'.txt') #YYMM/catYYMMxxxxx.pdf
+  else: # this is for rawid with no category, but we split category from metadata on the dot (if it has one)
+    schema='else'
+    txt_path = os.path.join('data', 'txt'
+           , j['_rawid'][:4].split("-")[0]
+           , j['arxiv_primary_category']['term'].split(".")[0]+j['_rawid']+'.txt') #YYMM/catYYMMxxxxx.pdf
   if os.path.isfile(txt_path): # some pdfs dont translate to txt
-    with open(txt_path, 'r') as f:
-      txt = f.read()
+    txt = read_txt_path(txt_path)
     if len(txt) > 1000 and len(txt) < 500000: # 500K is VERY conservative upper bound
       txt_paths.append(txt_path) # todo later: maybe filter or something some of them
       pids.append(idvv)
       print("read %d/%d (%s) with %d chars" % (n, len(db), idvv, len(txt)))
     else:
       print("skipped %d/%d (%s) with %d chars: suspicious!" % (n, len(db), idvv, len(txt)))
+      pass
   else:
     print("could not find %s in txt folder." % (txt_path, ))
+    print(schema)
 print("in total read in %d text files out of %d db entries." % (len(txt_paths), len(db)))
 
 # compute tfidf vectors with scikits
-v = TfidfVectorizer(input='content', 
-        encoding='utf-8', decode_error='replace', strip_accents='unicode', 
-        lowercase=True, analyzer='word', stop_words='english', 
+v = TfidfVectorizer(input='content',
+        encoding='utf-8', decode_error='replace', strip_accents='unicode',
+        lowercase=True, analyzer='word', stop_words='english',
         token_pattern=r'(?u)\b[a-zA-Z_][a-zA-Z0-9_]+\b',
-        ngram_range=(1, 2), max_features = max_features, 
+        ngram_range=(1, 2), max_features = max_features,
         norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=True,
         max_df=1.0, min_df=1)
 
 # create an iterator object to conserve memory
 def make_corpus(paths):
   for p in paths:
-    with open(p, 'r') as f:
-      txt = f.read()
-    yield txt
+    yield read_txt_path(p)      
 
 # train
 train_txt_paths = list(txt_paths) # duplicate
@@ -62,10 +97,41 @@ print("training on %d documents..." % (len(train_txt_paths), ))
 train_corpus = make_corpus(train_txt_paths)
 v.fit(train_corpus)
 
+# export texts for topic modelling
+corpus = make_corpus(txt_paths) # don't forget to rewind
+pattern = regex.compile('((?=[^!?.,\ ])\W|\d)+', regex.UNICODE)
+clean_txt=(pattern.sub(' ',str(text)[:1000]) for text in corpus)
+texts_df=pd.DataFrame(clean_txt, columns=['Text',])
+texts_df.to_excel('diego_texts.xlsx',index=True)
+del corpus
+
+# https://github.com/rafaelvalero/ParallelTextProcessing/blob/master/parallelizing_text_processing.ipynb
+num_cores = multiprocessing.cpu_count()
+num_partitions = num_cores-1
+# I like to leave some cores for other processes
+
+print('num_partitions',num_partitions)
+
+def parallelize_dataframe(df, func):
+    a = np.array_split(df, num_partitions)
+    del df
+    pool = Pool(num_cores)
+    sparse_mtrx = sp.vstack(pool.map(func, a), format='csr')
+    pool.close()
+    pool.join()
+    return sparse_mtrx
+
+def transform_func(data):
+    tfidf_matrix = v.transform(data["text"])
+    return tfidf_matrix
+
 # transform
 print("transforming %d documents..." % (len(txt_paths), ))
 corpus = make_corpus(txt_paths)
-X = v.transform(corpus)
+data_pd = pd.DataFrame(corpus)
+data_pd.rename(columns = {0:'text'},inplace = True)
+data_pd.head()
+X = parallelize_dataframe(data_pd, transform_func)
 print(v.vocabulary_)
 print(X.shape)
 
@@ -83,12 +149,10 @@ out['pids'] = pids # a full idvv string (id and version number)
 out['ptoi'] = { x:i for i,x in enumerate(pids) } # pid to ix in X mapping
 print("writing", Config.meta_path)
 safe_pickle_dump(out, Config.meta_path)
+del out
+del data_pd
 
-print("precomputing nearest neighbor queries in batches...")
-X = X.todense() # originally it's a sparse matrix
-sim_dict = {}
-batch_size = 200
-for i in range(0,len(pids),batch_size):
+def compute_batch(i):
   i1 = min(len(pids), i+batch_size)
   xquery = X[i:i1] # BxD
   ds = -np.asarray(np.dot(X, xquery.T)) #NxD * DxB => NxB
@@ -96,6 +160,13 @@ for i in range(0,len(pids),batch_size):
   for j in range(i1-i):
     sim_dict[pids[i+j]] = [pids[q] for q in list(IX[:50,j])]
   print('%d/%d...' % (i, len(pids)))
+
+print("precomputing nearest neighbor queries in batches...")
+X = X.todense() # originally it's a sparse matrix
+sim_dict = {}
+batch_size = 200
+Parallel( n_jobs=-1, prefer="threads", verbose=5)(
+    delayed(compute_batch)(i) for i in range(0,len(pids),batch_size))
 
 print("writing", Config.sim_path)
 safe_pickle_dump(sim_dict, Config.sim_path)
