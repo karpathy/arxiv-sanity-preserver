45c8c87ba274b118f6365d227a6d8841a0fe8e61
Dabbrivia <60330590+Dabbrivia@users.noreply.github.com>
2020-02-26 22:22:49 +0100

    run twitter_daemon.py, grep monitoring GETs out

--- run_sever.sh
+++ run_sever.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo mount /dev/nvme0n1 /data; sudo service mongod start; source ~/env/bin/activate; cd
- ~/arxiv-sanity-preserver/; python twitter_daemon.py 2>&1 1>/dev/null &; python serve.py --prod --port 8080;
- 
+sudo mount /dev/nvme0n1 /data; sudo service mongod start 
+source ~/env/bin/activate; cd ~/arxiv-sanity-preserver/
+python twitter_daemon.py 2>&1 1>/dev/null &
+python serve.py --prod --port 8080 2>&1|grep -v "200 GET / "
