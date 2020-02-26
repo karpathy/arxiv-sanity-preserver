1ea06e73bfd4afaf54a57a113c000518f384c5f3
Dabbrivia <60330590+Dabbrivia@users.noreply.github.com>
2020-02-26 08:07:54 +0100

    run twitter daemon in background

--- run_sever.sh
+++ run_sever.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo mount /dev/nvme0n1 /data; sudo service mongod start; source ~/env/bin/activate; cd
- ~/arxiv-sanity-preserver/; python serve.py --prod --port 8080;
+ ~/arxiv-sanity-preserver/; python twitter_daemon.py 2>&1 1>/dev/null &; python serve.py --prod --port 8080;
+ 
