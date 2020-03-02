dcbc6799460fceaec77d88d98ddfb5f29f7852f5
Amanjeev Sethi <160476+amanjeev@users.noreply.github.com>
2020-03-02 07:05:26 -0500

    Fix: Import error with Werkzeug

--- serve.py
+++ serve.py
@@ -12,7 +12,7 @@ from hashlib import md5
 from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash, _app_ctx_stack
 from flask_limiter import Limiter
-from werkzeug import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 import pymongo
 
 from utils import safe_pickle_dump, strip_version, isvalidid, Config
