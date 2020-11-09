44dbc0a2f95533feff5481523e5fcb51c2d62ddb
Ezra Ablaza <ezra.ablaza.inbox@gmail.com>
2020-11-08 23:37:39 -0600

    Enable csv creation from dynamodb streams

--- lambdas/download_pdfs.py
+++ lambdas/download_pdfs.py
@@ -1,5 +1,7 @@
 import boto3
 import logging
+import csv
+from datetime import datetime
 from botocore.exceptions import ClientError
 from boto3.dynamodb.types import TypeDeserializer
 
@@ -8,12 +10,19 @@ TABLE = None
 S3CLIENT = None
 DESERIALIZER = None
 
-def upload_file(file_name, s3, bucket):
+def upload_file(target_names, s3, bucket_details):
     # upload file to S3 bucket
     
-    object_name = file_name
+    if sub_folder := bucket_details['folder']:
+        object_name = sub_folder + "/{}".format(target_names['dest']) 
+    else:
+        object_name = target_names['dest']
     try:
-        response = s3.upload_life(file_name, bucket, object_name)
+        response = s3.upload_file(
+            target_names['src'], 
+            bucket_details['bucket'], 
+            object_name
+        )
     except ClientError as e:
         logging.error(e)
         return False
@@ -23,9 +32,41 @@ def upload_file(file_name, s3, bucket):
 def process_records(event):
     # extract entry metadata
     for record in event['Records']:
-        yield {
-            key: DESERIALIZER.deserialize(val) for key, val in record['dynamodb']['NewImage'].items()
+        if record['eventName'] == "INSERT":
+            yield {
+                key: DESERIALIZER.deserialize(val) for key, val in record['dynamodb']['NewImage'].items()
+            }
+        
+def write_entries_to_csv(event, s3):
+    # create a csv in an s3 bucket containing the entry data recorded in the event
+    now = datetime.now()
+    # file name uses time stamp common to all the records in the event
+    file_name = now.strftime("%a_%b_%d_%y_%H%M%S.%f.csv")
+    local_file_name = "/tmp/" + file_name
+    headed = False
+    with open(local_file_name, "w") as csv_file:
+        for data in process_records(event):
+            # start the dict writer and add fields if this is the first entry
+            if not headed:
+                field_names = [key for key in data.keys()]
+                entry_writer = csv.DictWriter(csv_file, field_names)
+                entry_writer.writeheader()
+                headed = True
+            # write info
+            entry_writer.writerow(data)
+    # upload the file
+    upload_file(
+        {
+            'src': local_file_name,
+            'dest': file_name
+        },
+        s3,
+        {
+            'bucket': 'asp2-file-bucket',
+            'folder': 'csv'
         }
+    )
+
 
 def main(event, context):
     # load table
@@ -39,11 +80,14 @@ def main(event, context):
     if DESERIALIZER is None:
         DESERIALIZER = TypeDeserializer()
     # logging
-    logging.root.setLevel(logging.NOTSET) 
+    logging.root.setLevel(logging.INFO) 
 
     # process records
     try:
-        for link in process_records(event):
-            logging.info(link)
-    except Exception as e:
-        logging.error(e)
+        logging.info(event['Records'])
+        logging.info("Processing %i records..." % len(event['Records']))
+        write_entries_to_csv(event, S3CLIENT)
+    except ClientError as cerr:
+        logging.error(cerr)
+    except Exception as ex:
+        logging.error(ex)
--- lambdas/fetch_papers.py
+++ lambdas/fetch_papers.py
@@ -107,76 +107,29 @@ def extract_info(entries):
         except Exception:
             raise
 
-def find_in_table(rawid, table):
-    # find entry in table with the rawid
-    try:
-        result = table.query(
-            KeyConditionExpression=Key('rawid').eq(rawid),
-            Limit=1
-        )
-        if result['Count'] == 0:
-            return {}
-        return result['Items'][0]
-    except ClientError as e:
-        logging.error(e.response['Error']['Message']) 
-
-def insert_into_table(entry_data, table, nums):
-    # insert entry_data into the table using its rawid if:
-    #   the rawid doesn't exist, or
-    #   the version of the entry_data is more recent than the one stored
-    rawid = entry_data['rawid']
-    version = entry_data['version']
-    if item := find_in_table(rawid, table):
-        if item['version'] >= version:     
-            nums['skipped'] += 1
-            return
-        else:
-            nums['updated'] += 1
-    else:
-        nums['added'] += 1
-    table.put_item(
-        Item=entry_data
-    )
-
 def main(event, context):
     # load table
     global TABLE
     if TABLE is None:
         TABLE = boto3.resource('dynamodb').Table('asp2-fetch-results')   
     # logging
-    logging.root.setLevel(logging.NOTSET) 
+    logging.root.setLevel(logging.INFO) 
 
     # start fetching and parsing
     args = parse_args(event)
     logging.info("Searching arXiv for %s\nusing these options: %s" % (args.data['search_query'], args.data))
     
     
-    total_nums = {
-        'added': 0,
-        'updated': 0,
-        'skipped': 0
-    }
     try:
         for parsed_resp in fetch_entries(args):
-            nums = {
-                'added': 0,
-                'updated': 0,
-                'skipped': 0
-            }
             # check response
             if not hasattr(parsed_resp, 'entries'):
                 raise Exception("No entries found in response.")
             # attempt an entry or replacement
-            for entry_data in extract_info(parsed_resp.entries):
-                insert_into_table(entry_data, TABLE, nums)
-            # show numbers
-            logging.info("Entries added: %i\nEntries updated: %i\nEntries skipped: %i" % 
-                (nums['added'], nums['updated'], nums['skipped']))
-            # add to totals
-            total_nums = {k: total_nums[k] + nums[k] for k in set(nums)}
+            with TABLE.batch_writer() as batch:
+                for entry_data in extract_info(parsed_resp.entries):
+                    batch.put_item(Item=entry_data)
     except Exception as e:
         logging.error(e)
 
-    logging.info("\n**Totals**\nEntries added: %i\nEntries updated: %i\nEntries skipped: %i" % 
-        (total_nums['added'], total_nums['updated'], total_nums['skipped']))
 
