232b99037fefbb2b62928d69d5cef005161614fb
profection <1124186046@qq.com>
2021-10-06 11:43:57 +0800

    Create analyze.py
    calculate tfidf using summary instead of entire text
    calculate tfidf using old vocab to make train faster
    refresh all of tfidf datas on first day of month to fix vocab
    calculate similarity using updating strategy instead of recalculating to save time
    change similarity object structure

--- analyze.py
+++ analyze.py
@@ -2,100 +2,202 @@
 Reads txt files of all papers and computes tfidf vectors for all papers.
 Dumps results to file tfidf.p
 """
+import collections
 import os
 import pickle
-from random import shuffle, seed
-
+import re
 import numpy as np
+from random import shuffle, seed
 from sklearn.feature_extraction.text import TfidfVectorizer
+from utils import Config, safe_pickle_dump, is_first_day_of_month
 
-from utils import Config, safe_pickle_dump
 
-seed(1337)
-max_train = 5000 # max number of tfidf training documents (chosen randomly), for memory efficiency
-max_features = 5000
+def load_pids():
+    print('loading pids if exists:%s' % Config.meta_path)
 
-# read database
-db = pickle.load(open(Config.db_path, 'rb'))
+    try:
+        if os.path.exists(Config.meta_path):
+            meta = pickle.load(open(Config.meta_path, 'rb'))
+            pids = meta['pids']
+        else:
+            pids = []
+    except Exception as e:
+        print('error loading existing pids:')
+        print(e)
+        print('starting from an empty pids')
+        pids = []
 
-# read all text files for all papers into memory
-txt_paths, pids = [], []
-n = 0
-for pid,j in db.items():
-  n += 1
-  idvv = '%sv%d' % (j['_rawid'], j['_version'])
-  txt_path = os.path.join('data', 'txt', idvv) + '.pdf.txt'
-  if os.path.isfile(txt_path): # some pdfs dont translate to txt
-    with open(txt_path, 'r') as f:
-      txt = f.read()
-    if len(txt) > 1000 and len(txt) < 500000: # 500K is VERY conservative upper bound
-      txt_paths.append(txt_path) # todo later: maybe filter or something some of them
-      pids.append(idvv)
-      print("read %d/%d (%s) with %d chars" % (n, len(db), idvv, len(txt)))
-    else:
-      print("skipped %d/%d (%s) with %d chars: suspicious!" % (n, len(db), idvv, len(txt)))
-  else:
-    print("could not find %s in txt folder." % (txt_path, ))
-print("in total read in %d text files out of %d db entries." % (len(txt_paths), len(db)))
+    return pids
 
-# compute tfidf vectors with scikits
-v = TfidfVectorizer(input='content', 
-        encoding='utf-8', decode_error='replace', strip_accents='unicode', 
-        lowercase=True, analyzer='word', stop_words='english', 
-        token_pattern=r'(?u)\b[a-zA-Z_][a-zA-Z0-9_]+\b',
-        ngram_range=(1, 2), max_features = max_features, 
-        norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=True,
-        max_df=1.0, min_df=1)
 
-# create an iterator object to conserve memory
-def make_corpus(paths):
-  for p in paths:
-    with open(p, 'r') as f:
-      txt = f.read()
-    yield txt
+def load_vocab():
+    print('loading vocab if exists:%s' % Config.meta_path)
 
-# train
-train_txt_paths = list(txt_paths) # duplicate
-shuffle(train_txt_paths) # shuffle
-train_txt_paths = train_txt_paths[:min(len(train_txt_paths), max_train)] # crop
-print("training on %d documents..." % (len(train_txt_paths), ))
-train_corpus = make_corpus(train_txt_paths)
-v.fit(train_corpus)
+    try:
+        if os.path.exists(Config.meta_path):
+            meta = pickle.load(open(Config.meta_path, 'rb'))
+            vocab = meta['vocab']
+        else:
+            vocab = None
+    except Exception as e:
+        print('error loading existing vocab:')
+        print(e)
+        print('starting from an empty vocab')
+        vocab = None
 
-# transform
-print("transforming %d documents..." % (len(txt_paths), ))
-corpus = make_corpus(txt_paths)
-X = v.transform(corpus)
-print(v.vocabulary_)
-print(X.shape)
+    return vocab
 
-# write full matrix out
-out = {}
-out['X'] = X # this one is heavy!
-print("writing", Config.tfidf_path)
-safe_pickle_dump(out, Config.tfidf_path)
 
-# writing lighter metadata information into a separate (smaller) file
-out = {}
-out['vocab'] = v.vocabulary_
-out['idf'] = v._tfidf.idf_
-out['pids'] = pids # a full idvv string (id and version number)
-out['ptoi'] = { x:i for i,x in enumerate(pids) } # pid to ix in X mapping
-print("writing", Config.meta_path)
-safe_pickle_dump(out, Config.meta_path)
+def load_sim_dict():
+    print('loading sim_dict if exists:%s' % Config.sim_path)
 
-print("precomputing nearest neighbor queries in batches...")
-X = X.todense() # originally it's a sparse matrix
-sim_dict = {}
-batch_size = 200
-for i in range(0,len(pids),batch_size):
-  i1 = min(len(pids), i+batch_size)
-  xquery = X[i:i1] # BxD
-  ds = -np.asarray(np.dot(X, xquery.T)) #NxD * DxB => NxB
-  IX = np.argsort(ds, axis=0) # NxB
-  for j in range(i1-i):
-    sim_dict[pids[i+j]] = [pids[q] for q in list(IX[:50,j])]
-  print('%d/%d...' % (i, len(pids)))
+    try:
+        if os.path.exists(Config.sim_path):
+            sim_dict = pickle.load(open(Config.sim_path, 'rb'))
+        else:
+            sim_dict = {}
+    except Exception as e:
+        print('error loading existing sim_dict:')
+        print(e)
+        print('starting from an empty sim_dict')
+        sim_dict = {}
 
-print("writing", Config.sim_path)
-safe_pickle_dump(sim_dict, Config.sim_path)
+    return sim_dict
+
+
+def clear_cache():
+    if os.path.exists(Config.meta_path):
+        os.remove(Config.meta_path)
+    if os.path.exists(Config.tfidf_path):
+        os.remove(Config.tfidf_path)
+    if os.path.exists(Config.sim_path):
+        os.remove(Config.sim_path)
+
+
+# read all text files for all papers into memory,keep idx not change
+def data_preparing():
+    db = pickle.load(open(Config.db_path, 'rb'))
+    db = collections.OrderedDict(sorted(db.items(), reverse=True))
+
+    handled = 0
+    txts, pids, new_pids = [], load_pids(), []
+    tmp_pids = set()
+    for pid in pids:
+        tmp_pids.add(pid)
+    print('adding new pids...')
+    for pid, _ in db.items():
+        set_len_before = len(tmp_pids)
+        tmp_pids.add(pid)
+        if set_len_before != len(tmp_pids):
+            pids.append(pid)
+            new_pids.append(pid)
+            handled += 1
+        if handled != 0 and handled % 10000 == 0:
+            print('new %d pids added' % (handled))
+    print('new %d pids added' % (handled))
+    handled = 0
+    for pid in pids:
+        handled += 1
+
+        j = db[pid]
+        title = j['title']
+        authors = ' '.join([re.sub('\\s', '', re.sub('[^a-zA-Z]', '', x['name'])) for x in j['authors']])
+        terms = ' '.join([re.sub(';', ' ', re.sub('[^a-zA-Z0-9.;]', '', x['term'])) for x in j['tags']])
+        summary = j['summary']
+        txts.append(title + ' ' + authors + ' ' + terms + ' ' + summary)
+        print("read %d/%d (%s)" % (handled, len(db), pid))
+
+    return txts, pids, new_pids
+
+
+def get_tfidf(txts, pids, max_features, max_train):
+    vocab = load_vocab()
+
+    tfidf_vec = TfidfVectorizer(input='content',
+                                encoding='utf-8', decode_error='replace', strip_accents='unicode',
+                                lowercase=True, analyzer='word', stop_words='english',
+                                token_pattern=r'(?u)\b[a-zA-Z_][a-zA-Z0-9_]+\b',
+                                ngram_range=(1, 2), max_features=max_features,
+                                norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=True,
+                                max_df=1.0, min_df=1, vocabulary=vocab)
+
+    # train
+    train_txts = list(txts)  # duplicate
+    shuffle(train_txts)  # shuffle
+    train_txts = train_txts[:min(len(train_txts), max_train)]  # crop
+    print("training on %d documents..." % (len(train_txts),))
+    tfidf_vec.fit(train_txts)
+    # writing lighter metadata information into a separate (smaller) file
+    out = {'vocab': tfidf_vec.vocabulary_, 'idf': tfidf_vec._tfidf.idf_, 'pids': pids,
+           'ptoi': {x: i for i, x in enumerate(pids)}}
+    print("writing", Config.meta_path)
+    safe_pickle_dump(out, Config.meta_path)
+
+    # transform
+    print("transforming %d documents..." % (len(txts),))
+    X = tfidf_vec.transform(txts)
+    # write full matrix out
+    out = {}
+    out['X'] = X  # this one is heavy!
+    print("writing", Config.tfidf_path)
+    safe_pickle_dump(out, Config.tfidf_path)
+    return X
+
+
+# ignore affect of idf change
+def calc_sim(X, cur_pids_len, new_pids, max_sim=50, batch_size=200, scale=100000):
+    print("precomputing nearest neighbor queries in batches...")
+    sim_dict = load_sim_dict()
+
+    # update old
+    base = cur_pids_len - len(new_pids)
+    for i in range(0, base, batch_size):
+        i1 = min(base, i + batch_size)
+        print('update similarity %d → %d' % (i, i1))
+
+        xquery = X[i:i1]  # BxD
+        ds = -np.asarray(np.dot(X[base:], xquery.T) * scale, dtype=np.int32)  # NxD * DxB => NxB
+
+        partition_len = min(len(ds), max_sim)
+        indexes = np.argpartition(ds, partition_len - 1, axis=0)  # 2th parameter is partition idx so minus 1
+
+        for j in range(i1 - i):
+            topk_idx = indexes[:partition_len, j]
+            sims = (sim_dict[i + j] + [(q + base, ds[q][j]) for q in topk_idx])
+            sim_dict[i + j] = sorted(sims, key=lambda x: x[1])[:max_sim]
+
+    # add new
+    for i in range(base, cur_pids_len, batch_size):
+        i1 = min(cur_pids_len, i + batch_size)
+        print('add similarity %d → %d' % (i, i1))
+
+        xquery = X[i:i1]  # BxD
+        ds = -np.asarray(np.dot(X, xquery.T) * scale, dtype=np.int32)  # NxD * DxB => NxB
+
+        partition_len = min(len(ds), max_sim)
+        indexes = np.argpartition(ds, partition_len - 1, axis=0)  # 2th parameter is partition idx so minus 1
+
+        for j in range(i1 - i):
+            topk_idx = indexes[:partition_len, j]
+            sims = [(q, ds[q][j]) for q in topk_idx]
+            sim_dict[i + j] = sorted(sims, key=lambda x: x[1])
+
+    print("writing", Config.sim_path)
+    safe_pickle_dump(sim_dict, Config.sim_path)
+
+
+if __name__ == "__main__":
+    seed(1337)
+    max_train = 5000  # max number of tfidf training documents (chosen randomly), for memory efficiency
+    max_features = 5000
+
+    # fix update not consider idf effect for old data every month
+    if is_first_day_of_month():
+        clear_cache()
+
+    txts, pids, new_pids = data_preparing()
+
+    if len(new_pids) > 0:
+        X = get_tfidf(txts, pids, max_features, max_train)
+        X = X.todense()  # originally it's a sparse matrix
+        calc_sim(X, len(pids), new_pids)
