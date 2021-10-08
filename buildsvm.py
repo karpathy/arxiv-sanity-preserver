fe760fbbabdca687b6580c26a93e8ff8aaeeddc3
profection <1124186046@qq.com>
2021-10-09 07:53:37 +0800

    Update buildsvm.py
    make code more readable
    add run method for outer call

--- buildsvm.py
+++ buildsvm.py
@@ -1,68 +1,86 @@
-# standard imports
 import os
-import sys
 import pickle
-# non-standard imports
+
 import numpy as np
 from sklearn import svm
 from sqlite3 import dbapi2 as sqlite3
-# local imports
-from utils import safe_pickle_dump, strip_version, Config
-
-num_recommendations = 1000  # papers to recommend per user
-# -----------------------------------------------------------------------------
-
-if not os.path.isfile(Config.database_path):
-    print("the database file as.db should exist. You can create an empty database with sqlite3 as.db < schema.sql")
-    sys.exit()
-
-sqldb = sqlite3.connect(Config.database_path)
-sqldb.row_factory = sqlite3.Row  # to return dicts rather than tuples
+from utils import safe_pickle_dump, Config
 
 
-def query_db(query, args=(), one=False):
+def get_sqldb():
+    if not os.path.isfile(Config.database_path):
+        sqldb = sqlite3.connect(Config.database_path)
+        with open('schema.sql') as fp:
+            sqldb.executescript(fp.read())
+    else:
+        sqldb = sqlite3.connect(Config.database_path)
+    sqldb.row_factory = sqlite3.Row  # to return dicts rather than tuples
+    return sqldb
+
+
+def query_db(sqldb, query, args=(), one=False):
     """Queries the database and returns a list of dictionaries."""
     cur = sqldb.execute(query, args)
     rv = cur.fetchall()
     return (rv[0] if rv else None) if one else rv
 
 
-# -----------------------------------------------------------------------------
+def get_users(sqldb):
+    # fetch all users
+    users = query_db(sqldb, '''select * from user''')
+    print('number of users: ', len(users))
+    return users
 
-# fetch all users
-users = query_db('''select * from user''')
-print('number of users: ', len(users))
 
-# load the tfidf matrix and meta
-meta = pickle.load(open(Config.meta_path, 'rb'))
-out = pickle.load(open(Config.tfidf_path, 'rb'))
-X = out['X']
-X = X.todense().astype(np.float32)
+def get_libs(sqldb, user_id):
+    return query_db(sqldb, '''select * from library where user_id = ?''', [user_id])
 
-xtoi = {x: i for x, i in meta['ptoi'].items()}
 
-user_sim = {}
-for ii, u in enumerate(users):
-    print("%d/%d building an SVM for %s" % (ii, len(users), u['username'].encode('utf-8')))
-    uid = u['user_id']
-    lib = query_db('''select * from library where user_id = ?''', [uid])
-    pids = [x['paper_id'] for x in lib]  # raw pids without version
-    posix = [xtoi[p] for p in pids if p in xtoi]
+def get_tfidf():
+    meta = pickle.load(open(Config.meta_path, 'rb'))
+    out = pickle.load(open(Config.tfidf_path, 'rb'))
+    X = out['X']
+    X = X.todense().astype(np.float32)
 
-    if not posix:
-        continue  # empty library for this user maybe?
+    pids = meta['pids']
+    ptoi = meta['ptoi']
+    return pids, ptoi, X
 
-    print(pids)
-    y = np.zeros(X.shape[0])
-    for ix in posix: y[ix] = 1
 
-    clf = svm.LinearSVC(class_weight='balanced', verbose=False, max_iter=10000, tol=1e-6, C=0.1)
-    clf.fit(X, y)
-    s = clf.decision_function(X)
+def get_user_sim(sqldb, users, meta_pids, ptoi, X, num_recommendations):
+    user_sim = {}
+    for ii, u in enumerate(users):
+        print("%d/%d building an SVM for %s" % (ii, len(users), u['username'].encode('utf-8')))
+        user_id = u['user_id']
+        user_raw_pids = [x['paper_id'] for x in get_libs(sqldb, user_id)]  # raw pids without version
+        user_pid_idx = [ptoi[p] for p in user_raw_pids if p in ptoi]
 
-    sortix = np.argsort(-s)
-    sortix = sortix[:min(num_recommendations, len(sortix))]  # crop paper recommendations to save space
-    user_sim[uid] = [meta['pids'][ix] for ix in list(sortix)]
+        if not user_pid_idx:
+            continue  # empty library for this user maybe?
 
-print('writing', Config.user_sim_path)
-safe_pickle_dump(user_sim, Config.user_sim_path)
+        print(user_raw_pids)
+        y = np.zeros(X.shape[0])
+        for ix in user_pid_idx: y[ix] = 1
+
+        clf = svm.LinearSVC(class_weight='balanced', verbose=False, max_iter=10000, tol=1e-6, C=0.1)
+        clf.fit(X, y)
+        s = clf.decision_function(X)
+
+        sortix = np.argsort(-s)
+        sortix = sortix[:min(num_recommendations, len(sortix))]  # crop paper recommendations to save space
+        user_sim[user_id] = [meta_pids[ix] for ix in list(sortix)]
+
+    print('writing', Config.user_sim_path)
+    safe_pickle_dump(user_sim, Config.user_sim_path)
+
+
+def run():
+    num_recommendations = 1000  # papers to recommend per user
+
+    sqldb = get_sqldb()
+    meta_pids, ptoi, X = get_tfidf()
+    get_user_sim(sqldb, get_users(sqldb), meta_pids, ptoi, X, num_recommendations)
+
+
+if __name__ == '__main__':
+    run()
